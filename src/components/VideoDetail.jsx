6d1acfc327f3ce6305fe6d167069eae5061660d5
bakisultanov@gmail.com <bakisultanov@gmail.com>
2023-11-02 22:07:55 +0600

    cloned remote repo

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -28,7 +28,7 @@ const VideoDetail = () => {
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
-          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+          <Box sx={{ width: "100%", top: "86px" }}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
               {title}
